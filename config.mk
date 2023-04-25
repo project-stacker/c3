b3c3d0d66572600af329ea8f1f629a10e2695810
Andrei Aaron <aaaron@luxoft.com>
2023-04-25 08:24:38 +0000

    fix(publish): fix image publishing logic
    Signed-off-by: Andrei Aaron <aaaron@luxoft.com>

--- config.mk
+++ config.mk
@@ -7,6 +7,13 @@ DISTRO?=ubuntu	# other options: debian, rockylinux
 DISTRO_REL?=jammy	# other options: bullseye, 9
 LAYER_TYPE?=tar	# type of layer produced: tar or squashfs
 PUBLISH_URL?=	# URL where to publish the results, for example docker://localhost:5000/c3
-PUBLISH_EXTRA_ARGS= 	# string containing credentials and/or other situational arguments
+PUBLISH_EXTRA_ARGS= 	# string containing situational arguments
 PUBLISH_TAG?=	# if set, it will override the image specific tags
-PULL_EXTRA_ARGS=	# string containing credentials and/or other situational arguments
+PUBLISH_USERNAME?=	# if set, this username will be used to publish the images on the registry
+PUBLISH_PASSWORD?= 	# if set, this password will be used to publish the images on the registry
+PULL_EXTRA_ARGS?=	# string containing credentials and/or other situational arguments
+
+PUBLISH_CREDS :=
+ifneq ($(PUBLISH_USERNAME),)
+PUBLISH_CREDS := --username $(PUBLISH_USERNAME) --password $(PUBLISH_PASSWORD)
+endif
